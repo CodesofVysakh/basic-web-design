90a7e831985dd564f2e53caf27abc6dbca88c96a
CodesofVysakh <onvysakh309@gmail.com>
2023-04-22 23:36:19 +0530

    button for contact

--- src/components/screens/home-landing/Contact.jsx
+++ src/components/screens/home-landing/Contact.jsx
@@ -97,10 +97,10 @@ function Contact() {
                                 <div class="sent-message">
                                     Your message has been sent. Thank you!
                                 </div>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit">Send Message</button>
                             </div> */}
+                            <div class="submit-button">
+                                <button type="submit">Send Message</button>
+                            </div>
                         </form>
                     </div>
                 </div>
